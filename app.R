df528c04842bfc37c337dea6818e60cdd5a56728
Sebastian Tacora <44708232+Stacora@users.noreply.github.com>
2023-09-10 00:56:53 -0500

    Update app.R
    The app is now more functional. We can see our amount of USD dollars in total, which will self update whenever the user adds a new record.

--- app.R
+++ app.R
@@ -9,7 +9,7 @@ generate_empty = function(NAs = F){
                    Amount = NA,
                    Currency = NA,
                    USD_price = NA,
-                   USD_amaount = NA,
+                   USD_amount = NA,
                    Description = NA,
                    In_Out = NA)
     return(a)
@@ -18,7 +18,7 @@ generate_empty = function(NAs = F){
                 Amount = character(0),
                 Currency = character(0),
                 USD_price = character(0),
-                USD_amaount = character(0),
+                USD_amount = character(0),
                 Description = character(0),
                 In_Out = character(0))
   return(a)
@@ -26,6 +26,10 @@ generate_empty = function(NAs = F){
 
 
 new_Data = generate_empty()
+
+## Padronizing the in_out variable
+values_inout = c('in', 'out', 'earned', 'spent', 1, -1)
+
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
@@ -39,7 +43,7 @@ ui <- fluidPage(
                   actionButton('newDoc', 'New file'))
            ),
     column(4,
-           textOutput('totalValue'))
+           tags$h1(textOutput('totalValue')))
   ),
   DT::dataTableOutput('mySheet'),
   fluidRow(
@@ -53,12 +57,24 @@ ui <- fluidPage(
            actionButton('addline', 'Add Line')),
     column(1,
            br(),
-           actionButton('submitLine', 'Submit Line'))
+           downloadButton('downloadData', 'Download'))
   )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
+  
+  ## creating a notification function personalized
+  show_semaforo = function(message, RedYellowGreenBlue = NULL){
+    messagetype = c('error', 'warning', 'success', 'message')
+    colorme = messagetype[RedYellowGreenBlue]
+    if(is.na(colorme)){
+      showNotification(message)
+    }else{
+      showNotification(message, type = colorme)
+    }
+    
+  }
   
   ## prepare new data
   observeEvent(input$newDoc, {
@@ -77,13 +93,19 @@ server <- function(input, output) {
       theFile$data
     }
   },
-  editable = list(target = 'cell'), 
+  editable = list(target = 'cell', desible = list(columns = c(5))), 
   server = T,
   selection = 'none',
   class = 'cell-border stripe')
   
   addme_lines = reactive({
     new_line = generate_empty(T)
+    dateit = strptime(as.character(Sys.Date()), '%Y-%m-%d')
+    dateit = format(dateit, '%d-%m-%Y')
+    new_line[['Date']] = dateit
+    new_line[['Amount']] = sprintf('%.2f', 0)
+    new_line[['USD_price']] = sprintf('%.2f', 0)
+    new_line[['USD_amount']] = sprintf('%.2f', 0)
     
     theFile$data <<- rbind(theFile$data, new_line)
     updateNumericInput(inputId = 'indexToDrop', value = nrow(theFile$data))
@@ -93,7 +115,7 @@ server <- function(input, output) {
   observeEvent(input$addline, {
     if(!exists('theFile')){
       # in case the file doesn't exist, a notification will appear
-      showNotification('No data was submited or created yet.')
+      show_semaforo('No data was submited or created yet.', 1)
     }else{
       addme_lines()
       # print(theFile$data)
@@ -103,9 +125,9 @@ server <- function(input, output) {
   observeEvent(input$dropline, {
     
     if(!exists('theFile')){
-      showNotification('No data was submited or created yet.')
+      show_semaforo('No data was submited or created yet.', 1)
     }else if(is.null(theFile$data) | (nrow(theFile$data) == 0)){
-      showNotification('No data was submited or created yet.')
+      show_semaforo('No data was submited or created yet.', 1)
     }else if(nrow(theFile$data) == 1){
       theFile$data <<- new_Data
     }else{
@@ -137,7 +159,6 @@ server <- function(input, output) {
     value_dt = info$value
     
     
-    
     ## Making sure the in_out variable is either 'in' or 'out'
     ## or 'earned' 'spent' or '1' '-1'
     c_names = colnames(new_Data)[col_dt]
@@ -146,17 +167,6 @@ server <- function(input, output) {
     ## valordt, c_names, row_dt, col_dt
     list_values = list(value_dt, c_names)
     evaluate_data[[c_names]](list_values)
-    
-    # if(c_names == 'In_Out'){
-    #   values_inout = c('in', 'out', 'earned', 'spent', 1, -1)
-    #   value_dt = stringr::str_to_lower(value_dt)
-    #   if(!(value_dt %in% values_inout)){
-    #     message_me = paste('You must input: in, out, earned, spent, 1 or -1 ')
-    #     showNotification(message_me)
-    #     return(NULL)
-    #   }
-    # }
-    
     
     pivot = theFile$data
     
@@ -181,7 +191,8 @@ server <- function(input, output) {
       gotrep = grepl(pattern = '^\\d{2}/\\d{2}/\\d{4}$', x = fecha)
       
       if(!gotrep){ ## Verifying that the date follows the global pattern date
-        showNotification('Date input must be in this numerical format: dd/mm/yyyy')
+        show_semaforo('Date input must be in this numerical format: dd/mm/yyyy', 
+                      2)
         return(NULL)
       }else{
         dayMonth = c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31)
@@ -199,57 +210,122 @@ server <- function(input, output) {
         
         ## The year must be greater than 2000
         if(date_num[3] < 2000){
-          showNotification('Please input a date after 01/01/2000')
+          show_semaforo('Please input a date after 01/01/2000', 2)
           return(NULL)
         }
         
         ## month data must be between 1 and 12
         if(!(date_num[2] %in% 1:12)){
-          showNotification('The month must be between 1 and 12')
+          show_semaforo('The month must be between 1 and 12', 2)
           return(NULL)
         }
         
         ## day must be grater or equal to 1
         if(date_num[1] < 1){
-          showNotification('The day must be no less than 01.')
+          show_semaforo('The day must be no less than 01.' , 2)
         }
         
         ## For the second month in a leap year
         if(date_num[2] == 2 & is_leap_year(date_num[3])){
           ## last day of february must be till 29
           ifelse(date_num[1] > 29, 
-                 showNotification('The day must be less than 29'),
+                 show_semaforo('The day must be less than 29', 2),
                  return(NULL))
           
           ## else, must be till 30 or 31, depending on the month
         }else if(!(date_num[1] <= dayMonth[date_num[2]])){
           nota_date = paste('Input a valid day, must be bellow ', 
                             dayMonth[date_num[2]])
-          showNotification(nota_date)
+          show_semaforo(nota_date, 2)
           return(NULL)
         }
       }
     },
     Amount = function(value_list){
-      print('hello')
+      value = value_list[[1]] %>% as.numeric()
+      if(is.na(value)){
+        show_semaforo('The amount must be a number', 2)
+        return(NULL)
+      }
     },
     Currency = function(value_list){},
-    USD_price = function(value_list){},
+    USD_price = function(value_list){
+      value = value_list[[1]] %>% as.numeric()
+      if(is.na(value)){
+        show_semaforo('The USD_price must be a number', 2)
+        return(NULL)
+      }
+    },
+    USD_amount = function(value_list){
+      value = value_list[[1]] %>% as.numeric()
+      if(is.na(value)){
+        show_semaforo('The USD_amount must be a number', 2)
+        return(NULL)
+      }
+    },
     Description = function(value_list){},
     In_Out = function(value_list){
       value = value_list[[1]]
       c_name = value_list[[2]]
       ## Verifying that the value input is padronized
-      values_inout = c('in', 'out', 'earned', 'spent', 1, -1)
       value_dt = stringr::str_to_lower(value)
       if(!(value_dt %in% values_inout)){
         message_me = paste('You must input: in, out, earned, spent, 1 or -1 ')
-        showNotification(message_me)
+        show_semaforo(message_me, 2)
         return(NULL)
       }
       
     }
   )
+  
+  output$downloadData <- downloadHandler(
+    filename = function() {
+      # Use the selected dataset as the suggested file name
+      paste0('myData', ".csv")
+    },
+    content = function(file) {
+      # Write the dataset to the `file` that will be downloaded
+      if(exists('theFile')){
+        if(nrow(theFile$data) == 0){
+          show_semaforo('There is no data to save', 1)
+          return(NULL)
+        }else{
+          write.csv(theFile$data, file)
+        }
+      }else{
+        show_semaforo('There is no data to save', 1)
+        return(NULL)
+      }
+    }
+  )
+  
+  inout_traductor = function(vec){
+    posiv = values_inout[c(1, 3, 5)]
+    negativ = values_inout[- c(1, 3, 5)]
+    result = ifelse(vec %in% posiv, 1, 
+                    ifelse(vec %in% negativ, -1, 0))
+  }
+  
+  totalUsD = eventReactive(input$mySheet_cell_edit,{
+    if(exists('theFile')){
+      if(nrow(theFile$data) == 0){
+        return('0,00 USD')
+      }else{
+        pivot = theFile$data[['USD_amount']] %>% as.numeric()
+        inout = theFile$data[['In_Out']] %>% str_to_lower() %>%
+          inout_traductor()
+        pivot = pivot * inout
+        
+        total = sprintf('%.2f', sum(pivot, na.rm = T))
+      }
+    }else{
+      return('0,00 USD')
+    }
+  })
+  
+  output$totalValue = renderText({
+    totalUsD()
+  })
   
 }
 
