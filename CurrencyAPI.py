fd789afb6ea2035f040ac5999835d9df33dcf398
Sebastian Tacora <44708232+Stacora@users.noreply.github.com>
2023-09-30 23:45:38 -0500

    Update CurrencyAPI.py
    Including new variables to the df_exchange data frame for organization.

--- CurrencyAPI.py
+++ CurrencyAPI.py
@@ -3,7 +3,9 @@ import pandas as pd
 #from datetime import datetime
 import datetime
 
-rpp = requests.get('https://openexchangerates.org/api/latest.json?app_id=################################')
+print('Downloading exchange info from the https://openexchangerates.org/api')
+
+rpp = requests.get('https://openexchangerates.org/api/latest.json?app_id=xxxxxxxxxxxxxxxxxxxxxxxxxx')
 
 datos = rpp.json()
 
@@ -28,6 +30,11 @@ datos['time'] = time_it
 datos['year'], datos['month'], datos['day'] = datos['timestamp_2'].year, datos['timestamp_2'].month, datos['timestamp_2'].day
 datos['hour'], datos['minute'], datos['second'] = datos['timestamp_2'].hour, datos['timestamp_2'].minute, datos['timestamp_2'].second
 
+
+df_exchange['Date'] = date_it
+df_exchange['Time'] = time_it
+df_exchange['timestamp'] = datos['timestamp']
+df_exchange['timestamp_2'] = datos['timestamp_2']
 metadatos = ['disclaimer', 'license', 'timestamp', 'base', 'date', 'time', 'year', 'month', 'day', 'hour', 'minute', 'second']
 
 datos_2 = {mykey : datos[mykey] for mykey in metadatos}
@@ -38,10 +45,14 @@ meta_values = datos_2.values()
 df_meta = {'keys' : meta_keys, 'values' : meta_values}
 df_meta = pd.DataFrame(df_meta)
 
-#print(df_exchange)
+
+### The finale datasets are
+#print(df_exchange.head())
 #print('###############################')
-#print(df_meta)
+#print(df_meta.head())
 
 ## Deleting objects
 del rates_USD, currency_keys, currency_values, rpp
 del date_it, time_it, datos, metadatos, datos_2, meta_keys, meta_values
+
+# df_exchange.to_csv('rates_test.csv')
